f54791052728fcd82b88211e1d348d5116f03327
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-02 10:09:46 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,15 +15,8 @@ node{
   
   stage('Test'){
     
-    checkout([$class: 'GitSCM',
-			  branches: [[name: '*/master']],
-			  userRemoteConfigs: [
-				[url: 'https://github.com/SujataKale97/webapp.git',
-				 credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c' ]
-			  ]
-			])  
-	  
-	bat 'gradle clean build'
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle-web-app.git'
+    bat 'gradle clean build'
     
     bat 'gradle test -Ppay'
   }
