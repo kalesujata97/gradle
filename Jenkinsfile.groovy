a667dce370ecb0f4d36696e8dbd83c1631a7d8f1
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-02 09:55:45 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ node{
 
   stage ('Build') {
    
-    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle-web-app.git' 
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle.git' 
         
     bat 'gradle clean build'
     
